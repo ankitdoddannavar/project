dba0eaa199c5260999b40674283dde73796f3776
ankitdoddannavar <dankit946@gmail.com>
2018-08-23 09:29:12 +0530

    //Arithmetic Operations

---
+++ Addition.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+
+class Addition
+{
+public static void main(String[] args)
+{
+int num1,num2,sum;
+
+{
+System.out.println("enter 2 numbers for add");
+Scanner in = new Scanner(System.in);
+num1 = in.nextInt();
+num2 = in.nextInt();
+sum = num1 + num2;
+
+System.out.println("sum of the numbers =" +sum);
+}
+}
+}
